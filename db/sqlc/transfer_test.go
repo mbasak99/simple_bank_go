e64261fada282f746653c46e47f9fb6127683157
Monark Basak <monarkb01@gmail.com>
2024-06-08 04:26:25 -0400

    Added tests to cover transfer sql code generation.

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,119 @@
+package simple_bank_db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/mbasak99/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1 Account, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, account1.ID)
+	require.Equal(t, arg.ToAccountID, account2.ID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt.Time)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt.Time, transfer2.CreatedAt.Time, time.Second)
+}
+
+func TestUpdateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer := createRandomTransfer(t, account1, account2)
+
+	arg := UpdateTransferParams{
+		ID:     transfer.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	transfer2, err := testQueries.UpdateTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, arg.ID, transfer2.ID)
+	require.Equal(t, arg.Amount, transfer2.Amount)
+	require.Equal(t, transfer.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, transfer2.ToAccountID)
+	require.WithinDuration(t, transfer.CreatedAt.Time, transfer2.CreatedAt.Time, time.Second)
+}
+
+func TestListTransfers(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+		createRandomTransfer(t, account2, account1)
+	}
+
+	arg := ListTransferParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	transfers, err := testQueries.ListTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.True(t, account1.ID == transfer.FromAccountID || account1.ID == transfer.ToAccountID)
+		require.True(t, account2.ID == transfer.FromAccountID || account2.ID == transfer.ToAccountID)
+	}
+}
+
+func TestDeleteTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	transfer := createRandomTransfer(t, account1, account2)
+	err := testQueries.DeleteTransfer(context.Background(), transfer.ID)
+
+	require.NoError(t, err)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+
+	require.EqualError(t, err, sql.ErrNoRows.Error()[5:]) // sql returns "sql: " while the err doesn't, therefore trim the sql err string
+	require.Empty(t, transfer2)
+}
