0892d5e22f9b1e68616e867475f5abe5ace210f9
Monark Basak <monarkb01@gmail.com>
2024-06-08 02:51:40 -0400

    Added tests for GetAccount.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package simple_bank_db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/mbasak99/simple_bank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -25,4 +26,33 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
 }
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account, account2)
+	require.Equal(t, account.ID, account2.ID)
+	require.Equal(t, account.Balance, account2.Balance)
+	require.Equal(t, account.Currency, account2.Currency)
+
+	require.WithinDuration(t, account.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+
+}
+
+func TestListAccounts(t *testing.T) {}
+
+func TestDeleteAccount(t *testing.T) {}
